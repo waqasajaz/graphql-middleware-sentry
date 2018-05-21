e74768945096e5dbd26abdbcb906821c2c111e47
Bruno Scheufler <bszerofive@gmail.com>
2018-05-21 22:26:31 +0200

    Added basic documentation
    Optimized build script with cross platform rm -rf version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config as raven, captureException, ConstructorOptions } from 'raven'
 import { IMiddleware } from 'graphql-middleware'
 
+// Options for graphql-middleware-sentry
 export interface Options {
   dsn: string
   config?: ConstructorOptions
@@ -13,12 +14,15 @@ export class SentryError extends Error {
 }
 
 export const sentry = (options: Options): IMiddleware => {
+  // Check if Sentry DSN is present
   if (!options.dsn) {
     throw new SentryError(`Missing dsn parameter in configuration.`)
   }
 
+  // Configure and install Raven
   raven(options.dsn, options.config).install()
 
+  // Return middleware resolver
   return async function(resolve, parent, args, ctx, info) {
     try {
       const res = await resolve(parent, args, ctx, info)
